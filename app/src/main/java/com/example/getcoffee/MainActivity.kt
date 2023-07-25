21a97332cce8084b8b47671d22fa09920345b0c7
Dong Cat <legorussiablock@gmail.com>
2023-07-25 21:24:20 +0700

    init

---
+++ app/src/main/java/com/example/getcoffee/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.getcoffee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.navigation.fragment.NavHostFragment
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Bottom navigator
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        // Switch fragments
+        bottomNav.setupWithNavController(navController)
+
+
+
+    }
+}
+

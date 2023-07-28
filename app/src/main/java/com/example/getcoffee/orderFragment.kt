0e480f6c7f314346d02b1330318b0b9d0c6358e4
Dong Cat <legorussiablock@gmail.com>
2023-07-29 00:48:16 +0700

    Delete excess

--- app/src/main/java/com/example/getcoffee/orderFragment.kt
+++ app/src/main/java/com/example/getcoffee/orderFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HomePageFragment.newInstance] factory method to
+ * Use the [homeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomePageFragment : Fragment() {
+class orderFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -49,7 +49,7 @@ class HomePageFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HomePageFragment().apply {
+            homeFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)

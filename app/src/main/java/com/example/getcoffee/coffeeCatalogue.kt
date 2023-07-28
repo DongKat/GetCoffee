f83556f08ab09bccf63b991ad077b1cbc7d7c8cb
Dong Cat <legorussiablock@gmail.com>
2023-07-29 02:49:34 +0700

    fix many

---
+++ app/src/main/java/com/example/getcoffee/coffeeCatalogue.kt
@@ -0,0 +1,8 @@
+package com.example.getcoffee
+
+import android.media.Image
+
+class coffeeCatalogue {
+    var image: Image? = null
+    var name: String? = null
+}
--- app/src/main/java/com/example/getcoffee/homeFragment.kt
+++ app/src/main/java/com/example/getcoffee/homeFragment.kt
@@ -1,10 +1,12 @@
 package com.example.getcoffee
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,26 +15,42 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HomePageFragment.newInstance] factory method to
+ * Use the [homeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomePageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class homeFragment : Fragment() {
+
+    private var profileButton: ImageView? = null;
+    private var cartButton: ImageView? = null;
+
+    private var List: ArrayList<coffeeCatalogue>? = null
+
+    private var loyaltyPoints: Int = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
+            profileButton = view?.findViewById(R.id.profileButton)
+            cartButton = view?.findViewById(R.id.cartButton)
+
+        }
+
+        profileButton?.setOnClickListener {
+            val intent = Intent(activity, profileActivity::class.java)
+            startActivity(intent)
+        }
+        cartButton?.setOnClickListener {
+            val intent = Intent(activity, myCartActivity::class.java)
+            startActivity(intent)
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home_page, container, false)
     }
@@ -49,7 +67,7 @@ class HomePageFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HomePageFragment().apply {
+            homeFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
